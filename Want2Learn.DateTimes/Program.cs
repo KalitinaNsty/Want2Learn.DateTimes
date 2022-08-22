aa595b01f485622762d7fbd96307b42ba86940d4
Анастасия Калитина <calitina.anastasya2010@yandex.ru>
2022-08-22 13:02:19 +0300

    Add project files.

---
+++ Want2Learn.DateTimes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Want2Learn.DateTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static DateTime GetRadomDate(int year)
+        {
+            Random rnd = new Random();
+            int month = rnd.Next(1, 13);
+            int dayOfMonth = DateTime.DaysInMonth(year, month);
+            int dayNumber = rnd.Next(1, dayOfMonth + 1);
+            DateTime date = new DateTime(year, month, dayNumber);
+            return date;
+        }
+    }
+}
