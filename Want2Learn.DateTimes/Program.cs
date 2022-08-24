c32590ef36491a6797362d93e0992970c2189c5e
Анастасия Калитина <calitina.anastasya2010@yandex.ru>
2022-08-24 14:11:39 +0300

    add GetBeginWeek

--- Want2Learn.DateTimes/Program.cs
+++ Want2Learn.DateTimes/Program.cs
@@ -17,5 +17,40 @@ namespace Want2Learn.DateTimes
             DateTime date = new DateTime(year, month, dayNumber);
             return date;
         }
+
+        static DateTime GetBeginWeek(DateTime date)
+        {
+            DayOfWeek day = date.DayOfWeek;
+            DateTime firstDayOfWeek = date;
+            if (day == DayOfWeek.Tuesday)
+            {
+                firstDayOfWeek = firstDayOfWeek.AddDays(-1);
+            }
+            if (day == DayOfWeek.Wednesday)
+            {
+                firstDayOfWeek = firstDayOfWeek.AddDays(-2);
+            }
+            if (day == DayOfWeek.Thursday)
+            {
+                firstDayOfWeek = firstDayOfWeek.AddDays(-3);
+            }
+            if (day == DayOfWeek.Friday)
+            {
+                firstDayOfWeek = firstDayOfWeek.AddDays(-4);
+            }
+            if (day == DayOfWeek.Saturday)
+            {
+                firstDayOfWeek = firstDayOfWeek.AddDays(-5);
+            }
+            if (day == DayOfWeek.Sunday)
+            {
+                firstDayOfWeek = firstDayOfWeek.AddDays(-6);
+            }
+            else
+            {
+                return firstDayOfWeek;
+            }
+            return firstDayOfWeek;
+        }
     }
 }
